4b17a135d0a1153ac9e7a6998e27329d6e9327d7
VioletForU <joshdacalos6@gmail.com>
2026-09-22 00:38:19 +0800

    edit horse3

--- games/derby-dash.js
+++ games/derby-dash.js
@@ -13,8 +13,8 @@
   var SPRITES = Array.isArray(window.DERBY_SPRITES) ? window.DERBY_SPRITES : [];
 
   var HORSES = [
-    { name: "Horse 1",   emoji: "🐎" },
-    { name: "Horse 2", emoji: "🐴" },
+    { name: "Horse 1",    emoji: "🐎" },
+    { name: "Horse 2",    emoji: "🐴" },
     { name: "Horse 3",    emoji: "🐎" },
     { name: "Horse 4",    emoji: "🐴" },
     { name: "Horse 5",    emoji: "🐎" }
